f0f681e2876ce3a74546b5d8daeafbb38338d64b
Quistis <suicidexhelia@gmail.com>
2024-01-03 12:45:47 +0500

    Поправил шаблон сортировки событий, в константы добавил объект с перечислением типов сортировки

--- src/view/trip-sort.js
+++ src/view/trip-sort.js
@@ -1,5 +1,23 @@
 import AbstractView from '../framework/view/abstract-view.js';
+import { SortTypes } from '../const.js';
 
+function createTripSortItemTemplate(sortType) {
+  return (
+    `<div class="trip-sort__item  trip-sort__item--${sortType}">
+      <input
+        id="sort-${sortType}"
+        class="trip-sort__input  visually-hidden"
+        type="radio"
+        name="trip-sort"
+        value="sort-${sortType}"
+        data-sort-type="${sortType}"
+        ${sortType === SortTypes.DAY ? 'checked' : ''}
+        ${sortType === SortTypes.EVENT || sortType === SortTypes.OFFERS ? 'disabled' : ''}>
+      <label class="trip-sort__btn" for="sort-${sortType}">${sortType}</label>
+    </div>`
+  );
+}
+/*
 function createTripSortTemplate() {
   return (
     `<form class="trip-events__trip-sort  trip-sort" action="#" method="get">
@@ -30,7 +48,14 @@ function createTripSortTemplate() {
   </form>`
   );
 }
-
+*/
+function createTripSortTemplate() {
+  return (
+    `<form class="trip-events__trip-sort  trip-sort" action="#" method="get">
+      ${Object.values(SortTypes).map((sortType) => createTripSortItemTemplate(sortType)).join('')}
+    </form>`
+  );
+}
 export default class TripSortView extends AbstractView {
   get template() {
     return createTripSortTemplate();
